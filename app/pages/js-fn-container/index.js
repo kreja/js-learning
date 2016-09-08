67424abb6d50616c6747f7f53222ad6ef3bfd62d
kaili.wkl <kaili.wkl@alibaba-inc.com>
2016-09-08 17:21:53 +0800

    update

--- app/pages/js-fn-container/index.js
+++ app/pages/js-fn-container/index.js
@@ -128,3 +128,89 @@ printToDom(makeVisa({name: 'mark', age: 20, money: 7000}));
 
 
 
+
+
+
+printDivider('IO，适用于：不得不跟外界交互。❗️跟外界交互就不是纯函数');
+// IO 的 value 总是一个函数
+var IO = function(f) {
+  this.__value = f;
+}
+IO.of = function(x) {
+  return new IO(function() {
+    return x;
+  });
+}
+IO.prototype.map = function(f) {
+  // return IO.of(f(this.__value)); // old way
+  // return IO.of(_.compose(f, this.__value)); // 等于下一条
+  return new IO(_.compose(f, this.__value)); // new way: 因为 __value 是函数，所以要 compose
+  // 这里其实并没有执行函数！只是把函数都 compose 起来生成了一个新函数赋值给 __value 得到一个新 IO
+  // 这些函数可能不是纯函数，但是现在并没有执行这些函数，而是得到新函数。那么 旧函数 -> 新函数 这个函数是纯函数！
+  // 我曹机智！
+  // 但是，这相当于把野兽关进了笼子，最后还是得把它放出来，不过可以甩锅给调用者，__.value() 就可以释放野兽。
+}
+
+// 用例
+//  io_window_ :: IO Window
+var io_window = IO.of(window);
+printToDom( io_window.map(function(win){ return win.innerWidth }).__value() ); // 为了打印出来所以用了 .__value()
+printToDom( io_window.map(_.prop('location')).map(_.prop('href')).map(_.split('/')).__value() );
+
+
+
+//  $ :: String -> IO [DOM]
+var $ = function(selector) {
+  return IO.of( document.querySelectorAll(selector) );
+}
+printToDom( $('#IO').map(_.head).map(function(div){ return div.innerHTML; }).__value() );
+
+
+
+
+
+
+
+
+
+printDivider('🔥IO 烧脑用例');
+
+// nmap :: a -> f -> a
+var nmap = _.curry(function(f, a){
+  return a.map(f);
+});
+
+//  url :: IO String
+var url = new IO(function() { return window.location.href; });
+
+//  toPairs =  String -> [[String]]
+var toPairs = _.compose(nmap(_.split('=')), _.split('&'));
+
+//  params :: String -> [[String]]
+var params = _.compose(toPairs, _.last, _.split('?'));
+
+//  findParam :: String -> IO Maybe [String]
+var findParam = function(key) {
+  // return map(params, url); // [["searchTerm","ok"],["age","10"]]
+  // return map( _.compose(_.filter(_.compose(_.equals(key), _.head)), params) , url); // [["searchTerm","ok"]]
+  return map(_.compose(Maybe.of, _.filter(_.compose(_.equals(key), _.head)), params), url);
+};
+
+// 调用 __value() 来运行它！
+printToDom( findParam("searchTerm").__value() ); // Maybe(['searchTerm', 'wafflehouse'])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
